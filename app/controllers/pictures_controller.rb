c255e05001fb42c129775d91ded9e01cc04b2a8d
tatsuhiko0123 <tatsuhiko.t.123@gmail.com>
2021-04-29 17:27:25 +0900

    コントローラー作成

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,16 @@
+class PicturesController < ApplicationController
+  def new
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def confirm
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'favorites/index'
+  get 'sessions/new'
+  get 'pictures/new'
+  get 'pictures/index'
+  get 'pictures/show'
+  get 'pictures/edit'
+  get 'pictures/confirm'
+  get 'users/new'
+  get 'users/show'
+  get 'users/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
