e37f4415436d772c90d46982027452342ced94b4
tatsuhiko0123 <tatsuhiko.t.123@gmail.com>
2021-04-29 20:58:31 +0900

    CRUD機能実装

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,16 +1,48 @@
 class PicturesController < ApplicationController
-  def new
-  end
-
+  before_action :set_picture, only: [:show, :edit, :update, :destroy]
   def index
+    @pictures = Picture.all
   end
+  def new
+    @picture = Picture.new
 
+  end
+  def create
+    @picture = current_user.pictures.build(picture_params)
+    if params[:back]
+      render :new
+    else
+      if @picture.save
+        redirect_to pictures_path, notice: "投稿を作成しました！"
+      else
+        render :new
+      end
+    end
+  end
   def show
   end
-
   def edit
   end
-
+  def update
+    if @picture.update(picture_params)
+      redirect_to pictures_path, notice: "投稿を編集しました！"
+    else
+      render :edit
+    end
+  end
+  def destroy
+    @picture.destroy
+    redirect_to pictures_path, notice:"投稿を削除しました！"
+  end
   def confirm
+    @picture = current_user.pictures.build(picture_params)
+    render :new if @picture.invalid?
+  end
+  private
+  def picture_params
+    params.require(:picture).permit(:title, :content, :user_id, :image, :image_cache)
+  end
+  def set_picture
+    @picture = Picture.find(params[:id])
   end
 end
