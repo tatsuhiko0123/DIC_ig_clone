b46ecd16741a7a928504d05e9994ffe0ef0184da
tatsuhiko0123 <tatsuhiko.t.123@gmail.com>
2021-04-29 23:11:20 +0900

    バリデーションの作成

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,4 +2,6 @@ class Picture < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
   mount_uploader :image, ImageUploader
+  validates :title, presence: true
+  validates :title, length: { in: 1..140 }
 end
